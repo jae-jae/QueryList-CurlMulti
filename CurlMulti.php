8f329a3c4c1dfea741f0247db8de9b9a290c0501
Jaeger <JaegerCode@gmail.com>
2017-09-27 23:25:49 +0800

    ok

--- CurlMulti.php
+++ CurlMulti.php
@@ -7,43 +7,73 @@
 
 namespace QL\Ext;
 
+use Ares333\CurlMulti\Core;
 use QL\Contracts\PluginContract;
 use QL\QueryList;
+use Closure;
 
 class CurlMulti implements PluginContract
 {
     protected $urls;
+    protected $queryList;
+    protected $successCallback;
+    protected $curl;
 
-    /**
-     * CurlMulti constructor.
-     * @param $urls
-     */
-    public function __construct($urls)
+
+    public function __construct(QueryList $queryList,$urls)
     {
+        $this->queryList = $queryList;
         $this->urls = $urls;
+        $this->curl = new Core();
     }
 
 
     public static function install(QueryList $queryList, ...$opt)
     {
         $queryList->bind('curlMulti',function (array $urls){
-
+            return new CurlMulti($this,$urls);
         });
     }
 
-    public function success()
+    public function success(Closure $callback)
     {
-
+        $this->successCallback = function ($r) use($callback){
+           $newQueryList = clone $this->queryList;
+           $newQueryList->setHtml($r['body']);
+           $callback($newQueryList,$r);
+        };
+        return $this;
     }
 
-    public function error()
+    public function error(Closure $callback)
     {
-
+        $this->curl->cbFail = $callback;
+        return $this;
     }
 
-    public static function start()
+    public function start(array $opt = [])
     {
+        $this->bindOpt($opt);
+        $this->addTask();
+        $this->curl->start();
+    }
 
+    protected function bindOpt($opt)
+    {
+        foreach ($opt as $key => $value) {
+            $this->curl->$key = $value;
+        }
+    }
+
+    protected function addTask()
+    {
+        foreach ($this->urls as $url) {
+            $this->curl->add([
+                'opt' => array(
+                    CURLOPT_URL => $url
+                )
+            ],$this->successCallback);
+        }
     }
 
 }
